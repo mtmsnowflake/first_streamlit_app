ac626cdef1618efbaa2326d2c1919eb8aeb5f7cd
Maite Tiemi de Moura <110480522+mtmsnowflake@users.noreply.github.com>
2022-08-02 14:58:59 -0300

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit
-
+import pandas 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('🍞 Breakfast')
 streamlit.text('espinafre, brocolis, couve, abobrinha, leite de oco, batata, salsäo, pimentäo')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
