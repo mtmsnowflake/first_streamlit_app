75ff45b7936e39527c127001a06d0a438a0079e5
Maite Tiemi de Moura <110480522+mtmsnowflake@users.noreply.github.com>
2022-08-02 14:56:02 -0300

    Update streamlit_app.py
    breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast')
+streamline.text('espinafre, brocolis, couve, abobrinha, leite de oco, batata, salsäo, pimentäo')
